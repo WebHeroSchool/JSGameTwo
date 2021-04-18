7d3ff5a6a1a69af47832e79a0e0c277c030bd500
Evelina <evelinfimova@gmail.com>
2021-04-18 18:22:28 +0300

    js-2 (2)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,27 @@ let num = '0';
 let score;
 
 const questionOne = {
-   question: 'Что такое солнце?',
-   answer: 'Планета',
-   currentAnswer: 'Звезда',
+   question: 'Кто впервые сформулировал закон всемирного тяготения?',
+   answer: 'Ньютон',
+   currentAnswer: 'Галилей',
 }
+
+const questionTwo = {
+   question: 'При увеличении массы одного из взаимодействующих тел в 5 раз сила всемирного тяготения: ',
+   answer: 'увеличится в 5 раз',
+   currentAnswer: 'уменьшиться в 5 раз',
+}
+
+const questionThree = {
+   question: 'Единица измерения силы в СИ -',
+   answer: 'H',
+   currentAnswer: 'H',
+}
+
+const questionFour = {
+   question: 'От чего зависит громкость звука',
+   answer: 'от амплитуды колебаний',
+   currentAnswer: 'от частоты и амплитуды',
+}
+
+
