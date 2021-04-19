dd0f6df9c285ace568275b6427852e0d2b879ac6
Evelina <evelinfimova@gmail.com>
2021-04-19 21:21:29 +0300

    js-3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
 let num = '0';
 let score;
 
-const questionOne = {
-   question: 'Кто впервые сформулировал закон всемирного тяготения?',
-   answer: 'Ньютон',
-   currentAnswer: 'Галилей',
-}
+const questions = [
+   {
+      question: 'Кто впервые сформулировал закон всемирного тяготения?',
+      answer: {
+         a: 'Аристотель',
+         b: 'Галилей',
+         c: 'Ньютон',
+      },
+      currentAnswer: 'b',
+      correctAnswer: 'c',
+   },
+   
+   {
+      question: 'При увеличении массы одного из взаимодействующих тел в 5 раз сила всемирного тяготения:',
+      answer: {
+         a: 'уменьшиться в 5 раз',
+         b: 'увеличится в 5 раз',
+         c: 'не изменится',
+      },
+      currentAnswer: 'a',
+      correctAnswer: 'b',
+   },
+   
+   {
+      question: 'Единица измерения силы в СИ -',
+      answer: {
+         a: 'F',
+         b: 'Дж',
+         c: 'H',
+      },
+      currentAnswer: 'c',
+      correctAnswer: 'c',
+   },
+   
+   {
+      question: 'От чего зависит громкость звука',
+      answer: {
+         a: 'от амплитуды колебаний',
+         b: 'от частоты и амплитуды',
+         c: 'от источника колебаний',
+      },
+      currentAnswer: 'c',
+      correctAnswer: 'a',
+   },
+]
 
-const questionTwo = {
-   question: 'При увеличении массы одного из взаимодействующих тел в 5 раз сила всемирного тяготения: ',
-   answer: 'увеличится в 5 раз',
-   currentAnswer: 'уменьшиться в 5 раз',
-}
-
-const questionThree = {
-   question: 'Единица измерения силы в СИ -',
-   answer: 'H',
-   currentAnswer: 'H',
-}
-
-const questionFour = {
-   question: 'От чего зависит громкость звука',
-   answer: 'от амплитуды колебаний',
-   currentAnswer: 'от частоты и амплитуды',
-}
+let correctAnswer = questions.forEach(function(item) {
+   if (item.correctAnswer === 'c') {
+      console.log(questions.indexOf(item) + ': ' + item.correctAnswer)
+   }
+});
 
 
